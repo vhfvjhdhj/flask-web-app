721097b0182f135e2c5b0c40f941dde3c4c816b1
vhfvjhdhj <2692372127@qq.com>
2025-09-27 17:58:48 +0800

    修改NEU

--- tests/test_app.py
+++ tests/test_app.py
@@ -14,7 +14,7 @@ class TestApp(unittest.TestCase):
     def test_hello_world(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode('utf-8'), 'Hello, World!')
+        self.assertEqual(response.data.decode('utf-8'), 'Hello, NEU!')
 
 if __name__ == '__main__':
     unittest.main()
